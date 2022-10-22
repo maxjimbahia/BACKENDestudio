7b27ca83b204ce99a86f92e5366cc062991d8004
maxjimbahia <104466391+maxjimbahia@users.noreply.github.com>
2022-10-22 01:57:03 -0300

    Add files via upload

--- tiendaropa/listarencards.php
+++ tiendaropa/listarencards.php
@@ -21,28 +21,25 @@
   <header>
     <ul class="nav nav-pills">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="listar.php">Tienda Ropa</a>
+        <a class="nav-link active" aria-current="page" href="listarencards.php">Tienda Ropa</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Busqueda</a>
         <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="listarencards.php">Busos</a></li>
-          <li><a class="dropdown-item" href="#">Nike</a></li>
-          <li><a class="dropdown-item" href="#">Mayor a $500</a></li>
+          <li><a class="dropdown-item" href="buzos.php">Busos</a></li>
+          <li><a class="dropdown-item" href="nike.php">Nike</a></li>
+          <li><a class="dropdown-item" href="mayora500.php">Mayor a $500</a></li>
           <li><hr class="dropdown-divider"></li>
           <li><a class="dropdown-item" href="#">Lista completa</a></li>
         </ul>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="listar.php">Todos los Registros</a>
       </li>
     </ul>
   </header>
   <main>
-    <h1>Tienda de ropa</h1>
-    <button type="submit"><a href="index.html">Inicio</a></button>
-    <button type="submit"><a href="listar.php">Listar ropa</a></button>
-    <button type="submit"><a href="agregar.html">Agregar ropa</a></button>
+    <h1>Tienda de Ropa</h1>
     <h2>Lista de ropa</h2>
     <p>La siguiente lista muestra los datos de la ropa actualmente en stock.</p>
 
@@ -51,32 +48,32 @@
         <div class="row">
 
           <?php
-          // 1) Conexion
-          $conexion=mysqli_connect("127.0.0.1", "root", "");
-          mysqli_select_db($conexion, "tiendaderopa");
+            // 1) Conexion
+            $conexion=mysqli_connect("127.0.0.1", "root", "");
+            mysqli_select_db($conexion, "escuela");
 
-          // 2) Preparar la orden SQL
-          // Sintaxis SQL SELECT
-          // SELECT * FROM nombre_tabla
-          // => Selecciona todos los campos de la siguiente tabla
-          // SELECT campos_tabla FROM nombre_tabla
-          // => Selecciona los siguientes campos de la siguiente tabla
-          $consulta = "SELECT * FROM `tiendaderopa`";
+            // 2) Preparar la orden SQL
+            // Sintaxis SQL SELECT
+            // SELECT * FROM nombre_tabla
+            // => Selecciona todos los campos de la siguiente tabla
+            // SELECT campos_tabla FROM nombre_tabla
+            // => Selecciona los siguientes campos de la siguiente tabla
+            $consulta = "SELECT * FROM tiendaderopa";
 
-          // 3) Ejecutar la orden y obtenemos los registros
-          $datos=mysqli_query($conexion, $consulta);
-          $fila=mysqli_result_array($datos);
-          
-          // 4) el while recorre todos los registros y genera una CARD PARA CADA UNA
-          
-          while ( $fila=mysqli_result_array($datos)) {?>
-            <div class="card col-sm-12 col-md-6 col-lg-3">
-              <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen'])?>" alt="" width="100px" height="100px")>
-              <a href="ver.php?id=<?php echo $reg['id'];?>" class="card-body">
-                <h3 class="card-title" style="width: 100%; font-size:25px;"><?php echo ucwords($reg['marca']) ?></h3>
-                <span>$ <?php echo $reg['precio']; ?></span>
-              </a>
-            </div>
+            // 3) Ejecutar la orden y obtenemos los registros
+            $datos = mysqli_query($conexion, $consulta);
+            
+            
+            // 4) el while recorre todos los registros y genera una CARD PARA CADA UNA
+            
+            while ($reg = mysqli_fetch_array($datos)) {?>
+              <div class="card col-sm-12 col-md-6 col-lg-3">
+                <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen'])?>" alt="" width="200px" height="200px")>
+                <a href="ver.php?id=<?php echo $reg['id'];?>" class="card-body">
+                  <h3 class="card-title" style="width: 100%; font-size:25px;"><?php echo ucwords($reg['marca']) ?></h3>
+                  <span>$ <?php echo $reg['precio']; ?></span>
+                </a>
+              </div>
 
           <?php } ?>
 
@@ -84,7 +81,7 @@
       </div>
     </main>
   </section>
-  <!-- JavaScript del bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- JavaScript del bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- tiendaropa/nike.php
+++ tiendaropa/nike.php
@@ -39,51 +39,45 @@
     </ul>
   </header>
     <main>
+  <h1>Tienda de ropa</h1>
+  <h2>Listado de ropa de la marca Nike</h2>
+  <p>La siguiente lista muestra los datos de la ropa actualmente en stock.</p>
 
-    <h1>Tienda de ropa</h1>
+  <section>
+    <div class="container">
+      <div class="row">
 
-    <h2>Lista de ropa</h2>
-    <p>La siguiente lista muestra los datos de la ropa actualmente en stock.</p>
-    <table class="table table-striped">
-      <tr>
-        <th >ID</th>
-        <th>TIPO DE ROPA</th>
-        <th>MARCA</th>
-        <th>TALLE</th>
-        <th>PRECIO</th>
-      </tr>
-    
-      <?php
-      // 1) Conexion
-          $conexion=mysqli_connect("127.0.0.1","root","");
-          mysqli_select_db($conexion, "escuela");
+        <?php
+        // 1) Conexion
+        $conexion = mysqli_connect("127.0.0.1", "root", "");
+        mysqli_select_db($conexion, "escuela");
 
-      // 2) Preparar la orden SQL
-      // Sintaxis SQL SELECT
-      // SELECT * FROM nombre_tabla
-      // => Selecciona todos los campos de la siguiente tabla
-      // SELECT campos_tabla FROM nombre_tabla
-      // => Selecciona los siguientes campos de la siguiente tabla
-        $consulta = "SELECT*FROM tiendaderopa";
+        // 2) Preparar la orden SQL
+        // Sintaxis SQL SELECT
+        // SELECT * FROM nombre_tabla
+        // => Selecciona todos los campos de la siguiente tabla
+        // SELECT campos_tabla FROM nombre_tabla
+        // => Selecciona los siguientes campos de la siguiente tabla
+        $consulta = "SELECT*FROM tiendaderopa WHERE marca='nike'";
 
-      // 3) Ejecutar la orden y obtenemos los registros
-        $datos=mysqli_query($conexion, $consulta);
-        
-      // 4) Mostrar los datos del registro
-      
-      while ($reg = mysqli_fetch_array($datos)) { ?>
-        <tr class="table">
-          <td><?php echo $reg['id']; ?></td>
-          <td><?php echo $reg['tipoderopa']; ?></td>
-          <td><?php echo $reg['marca']; ?></td>
-          <td><?php echo $reg['talle']; ?></td>
-          <td><?php echo $reg['precio']; ?></td>
-        </tr>
-      <?php } ?>
-    </table>
+        // 3) Ejecutar la orden y obtenemos los registros
+        $datos = mysqli_query($conexion, $consulta);
 
-      
-  
+        // 4) el while recorre todos los registros y genera una CARD PARA CADA UNA
+
+        while ($reg = mysqli_fetch_array($datos)) {?>
+          <div class="card col-sm-12 col-md-6 col-lg-3">
+            <img class="card-img-top" src="data:image/jpg;base64, <?php echo base64_encode($reg['imagen'])?>" alt="" width="200px" height="200px")>
+            <a href="ver.php?id=<?php echo $reg['id'];?>" class="card-body">
+              <h3 class="card-title" style="width: 100%; font-size:25px;"><?php echo ucwords($reg['marca']) ?></h3>
+              <span>$ <?php echo $reg['precio']; ?></span>
+            </a>
+          </div>
+        <?php } ?>
+
+      </div>
+    </div>
+  </section>
   <!-- JavaScript del bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
